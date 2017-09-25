a21c9413c99f7cf633763aca07979e26dd720826
Jessie Cai <tsai19930224@gmail.com>
2017-09-25 15:19:28 +0800

    c6

--- count.js
+++ count.js
@@ -1,7 +1,8 @@
 var age;
 function modifyCount (age,month) {
-    console.log("My age is " + age);
-    console.log("I was born in " + month);
+    console.log("Counting the countless " + age);
+    // console.log("My age is " + age);
+    // console.log("I was born in " + month);
 
 }
 
